0341e0d276d7a187a60361775b5fd20d9e2ec427
PAUL <you@example.com>
2022-03-15 17:36:45 +0300

    7 commit

--- blucon.js
+++ blucon.js
@@ -9,20 +9,27 @@ const navSlide = () =>{
     nav.classList.toggle('nav-active');
 
     navLinks.forEach((link, index) =>{
-      if(link.style.animation){
+      if (link.style.animation){
         link.style.animation = '';
-      }
-      else{
+      }else {
         link.style.animation = `navLinkFade 0.5s ease forwards $(index /7 + 0.3)s`
       }
-    
-   
      })
-
   });
 
+  navLinks.forEach(link => link.addEventListener("click", function(){
+    nav.classList.remove('nav-active');
+  }))
 
+  window.addEventListener("mousedown", function (e) {
+    const line1 = document.querySelector('.line1');
+    const line2 = document.querySelector('.line2');
+    const line3 = document.querySelector('.line3');
+
+    if (e.target !== burger && e.target !== nav && e.target !== line1 && e.target !== line2 && e.target !== line3){
+      nav.classList.remove('nav-active');
+    }
+  })
 }
 
 navSlide();
-
